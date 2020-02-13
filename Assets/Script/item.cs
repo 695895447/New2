5f5279e58eacb6dfd8ca8f0edffe623e09e2e72d
695895447 <57555072+695895447@users.noreply.github.com>
2020-02-14 04:59:58 +0800

    11

---
+++ Assets/Script/item.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class item : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public Vector3 itemStart_Pos; //物件的起始位置
+    public Vector3 Pos;           //要更改的座標
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        itemStart_Pos=GetComponent<Transform>().position;   //記錄物件的起始位置
+        Pos=itemStart_Pos;             //要更改的座標=起始位置
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position =eventData.position;     //物件跟着滑鼠移動
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.position=Pos;                 //物件位置=要更改的位置
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)     //進入Trigger 
+    {
+        if(collision.GetComponent<itemBox>().haveItem==false)  //如果格子上沒東西
+        {
+            Pos=collision.transform.position;               //要更改的位置=格子的位置
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)       //離開Trigger
+    {
+        Pos=itemStart_Pos;             //要更改的座標=起始位置
+    }
+}
---
+++ Assets/Script/itemBox.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class itemBox : MonoBehaviour
+{
+    public bool haveItem;        //記錄格子上有沒有物品
+    public string itemName;      //格子上物理的名稱
+    public item Item;
+
+    public void OnTriggerEnter2D(Collider2D collision)  //進入Trigger
+    {
+        if(GetComponent<itemBox>().haveItem==false)   //如果格子上沒東西
+        {
+            GetComponent<itemBox>().haveItem=true;    //itemBox的haveItem改為true
+            GetComponent<Image>().color=Color.blue;   //那一格改為顯示藍色
+            itemName = collision.ToString();
+            print(collision);
+        }
+        else
+        {
+            collision.GetComponent<Image>().color=Color.red;    //那一格改為顯示
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)       //離開Trigger
+    {
+        GetComponent<Image>().color=Color.white;   //那一格改為顯示白色
+        if(itemName == collision.ToString())
+        {
+            GetComponent<itemBox>().haveItem=false;    //itemBox的haveItem改為false
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+/*public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.GetComponent<itemBox>().haveItem==true)
+        {
+            GetComponent<Image>().color=Color.red;
+        }
+        else 
+        {
+            GetComponent<Image>().color=Color.blue;
+        }
+    }*/
